6b7cd8ccc696a7fc4e5344b89c4407ff41ebd303
nsk1608 <125968994+nsk1608@users.noreply.github.com>
2023-03-07 12:08:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,3 @@ if streamlit.button('Get fruit load list'):
   my_data_rows=get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('Thanks for adding ',add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
